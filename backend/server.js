3bebbafc7cd66c0d816f6ed42dc035a7e37d5429
mahidhar1 <mnyayapati@gmail.com>
2022-10-08 14:03:15 +0530

    Initial project setup. Initial routes

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on PORT ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
