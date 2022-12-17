9a7f2e161c7ffd793f07ae6179e2d29a7bf306bb
mahidhar1 <mnyayapati@gmail.com>
2022-12-17 16:49:23 +0530

    Backend completed

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
+} = require("../controllers/userController");
+const protect = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/getUser", protect, getUser);
+router.get("/loggedin", loginStatus);
+router.patch("/updateuser", protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
+router.post("/forgotpassword", forgotPassword);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,40 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// app files
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactusRoute = require("./routes/contactusRoute");
+const errorHandler = require("./middlewares/errorMiddleware");
+const { application } = require("express");
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+// static file upload from a folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//Routes middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactusRoute);
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
+//Error middleware
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 // Connect to DB and start server
